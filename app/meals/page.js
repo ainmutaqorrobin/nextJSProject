fa40ee6499ba3806e74c3e70b886f9fb124a6033
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-07-23 10:59:43 +0800

    meals component added

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,23 @@
 import Link from "next/link";
+import styles from "./page.module.css";
+import MealsGrid from "../components/meals/meals-grid";
 
-export default function Meals() {
+export default function MealsPage() {
   return (
-    <main>
-      <h1>This is main meal page</h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, create{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself.</p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
