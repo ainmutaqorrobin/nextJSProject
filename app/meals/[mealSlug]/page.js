f45ec2b21e1167e2a65959a7d0fcfec434f4866b
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-07-24 08:19:33 +0800

    added meal details page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,34 @@
-export default function MealsDetail({ params }) {
+import Image from "next/image";
+import styles from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+
+export default async function MealsDetail({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if(!meal) notFound();
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
   return (
-    <main>
-      <h1>This is meal {params.slug}</h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
